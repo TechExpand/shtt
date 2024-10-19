6b04a71ded8a8c5b9dd84f8a44600c7d7cb1adc2
TechExpand <dailydevo9@gmail.com>
2024-10-19 08:36:06 +0100

    done

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,41 +1,78 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 function ormConfig(): TypeOrmModuleOptions {
-    const commonConf = {
-        SYNCRONIZE: false,
-        ENTITIES: [__dirname + '/domain/*.entity{.ts,.js}'],
-        MIGRATIONS: [__dirname + '/migrations/**/*{.ts,.js}'],
-        CLI: {
-            migrationsDir: 'src/migrations',
-        },
-        MIGRATIONS_RUN: true,
+  const commonConf = {
+    SYNCRONIZE: false,
+    ENTITIES: [__dirname + '/domain/*.entity{.ts,.js}'],
+    MIGRATIONS: [__dirname + '/migrations/**/*{.ts,.js}'],
+    CLI: {
+      migrationsDir: 'src/migrations',
+    },
+    MIGRATIONS_RUN: true,
+  };
+
+  let ormconfig: TypeOrmModuleOptions = {
+    // name: 'default',
+    // type: 'sqlite',
+    // database: '../target/db/sqlite-dev-db.sql',
+
+    name: 'shuttle',
+    type: 'postgres',
+    database: 'defaultdb',
+    host: 'pg-42f8939-pikamsell-3fdc.k.aivencloud.com',
+    port: 18306,
+    username: 'avnadmin',
+    password: 'AVNS_3ANekNkSZAQA7vhn0Oh',
+    logging: true,
+    synchronize: true,
+    entities: commonConf.ENTITIES,
+    // migrations: commonConf.MIGRATIONS,
+    // // cli: commonConf.CLI,
+    // migrationsRun: commonConf.MIGRATIONS_RUN,
+  };
+
+  // {
+  //     // name: 'default',
+  //     // type: 'sqlite',
+  //     // database: '../target/db/sqlite-dev-db.sql',
+
+  //     name: 'shuttlenet',
+  //     type: 'postgres',
+  //     database: 'initial_db',
+  //     host: 'shuttlenet.cnwkmxf2kced.eu-west-1.rds.amazonaws.com',
+  //     port: 5432,
+  //     username: 'shuttlenet',
+  //     password: 'shuttlenet126',
+  //     logging: true,
+  //     synchronize: true,
+  //     entities: commonConf.ENTITIES,
+  //     // migrations: commonConf.MIGRATIONS,
+  //     // // cli: commonConf.CLI,
+  //     migrationsRun: commonConf.MIGRATIONS_RUN,
+  // };
+
+  if (process.env.BACKEND_ENV === 'prod') {
+    ormconfig = {
+      // name: 'default',
+      // type: 'sqlite',
+      // database: '../target/db/sqlite-dev-db.sql',
+
+      name: 'shuttle',
+      type: 'postgres',
+      database: 'defaultdb',
+      host: 'pg-42f8939-pikamsell-3fdc.k.aivencloud.com',
+      port: 18306,
+      username: 'avnadmin',
+      password: 'AVNS_3ANekNkSZAQA7vhn0Oh',
+      logging: true,
+      synchronize: true,
+      entities: commonConf.ENTITIES,
+      // migrations: commonConf.MIGRATIONS,
+      // // cli: commonConf.CLI,
+      // migrationsRun: commonConf.MIGRATIONS_RUN,
     };
 
-    let ormconfig: TypeOrmModuleOptions =  {
-            // name: 'default',
-            // type: 'sqlite',
-            // database: '../target/db/sqlite-dev-db.sql',
-    
-            name: 'shuttle',
-            type: 'postgres',
-            database: 'onnasmdy',
-            host: 'tyke.db.elephantsql.com',
-            port: 5432,
-            username: 'onnasmdy',
-            password: 'uLDLWS0wMgzgF3z4S4wIlvoFH-gm480I',
-            logging: true,
-            synchronize: true,
-            entities: commonConf.ENTITIES,
-            // migrations: commonConf.MIGRATIONS,
-            // // cli: commonConf.CLI,
-            // migrationsRun: commonConf.MIGRATIONS_RUN,
-        };
-    
     // {
-    //     // name: 'default',
-    //     // type: 'sqlite',
-    //     // database: '../target/db/sqlite-dev-db.sql',
-
     //     name: 'shuttlenet',
     //     type: 'postgres',
     //     database: 'initial_db',
@@ -43,75 +80,36 @@ function ormConfig(): TypeOrmModuleOptions {
     //     port: 5432,
     //     username: 'shuttlenet',
     //     password: 'shuttlenet126',
-    //     logging: true,
-    //     synchronize: true,
+    //     logging: false,
+    //     synchronize: commonConf.SYNCRONIZE,
     //     entities: commonConf.ENTITIES,
-    //     // migrations: commonConf.MIGRATIONS,
+    //     migrations: commonConf.MIGRATIONS,
     //     // // cli: commonConf.CLI,
     //     migrationsRun: commonConf.MIGRATIONS_RUN,
     // };
+  }
 
-
-    if (process.env.BACKEND_ENV === 'prod') {
-        ormconfig = {
-            // name: 'default',
-            // type: 'sqlite',
-            // database: '../target/db/sqlite-dev-db.sql',
-    
-            name: 'shuttle',
-            type: 'postgres',
-            database: 'onnasmdy',
-            host: 'tyke.db.elephantsql.com',
-            port: 5432,
-            username: 'onnasmdy',
-            password: 'uLDLWS0wMgzgF3z4S4wIlvoFH-gm480I',
-            logging: true,
-            synchronize: true,
-            entities: commonConf.ENTITIES,
-            // migrations: commonConf.MIGRATIONS,
-            // // cli: commonConf.CLI,
-            // migrationsRun: commonConf.MIGRATIONS_RUN,
-        };
-        
-        // {
-        //     name: 'shuttlenet',
-        //     type: 'postgres',
-        //     database: 'initial_db',
-        //     host: 'shuttlenet.cnwkmxf2kced.eu-west-1.rds.amazonaws.com',
-        //     port: 5432,
-        //     username: 'shuttlenet',
-        //     password: 'shuttlenet126',
-        //     logging: false,
-        //     synchronize: commonConf.SYNCRONIZE,
-        //     entities: commonConf.ENTITIES,
-        //     migrations: commonConf.MIGRATIONS,
-        //     // // cli: commonConf.CLI,
-        //     migrationsRun: commonConf.MIGRATIONS_RUN,
-        // };
-    }
-
-    if (process.env.BACKEND_ENV === 'test') {
+  if (process.env.BACKEND_ENV === 'test') {
     ormconfig = {
-        // name: 'default',
-        // type: 'sqlite',
-        // database: '../target/db/sqlite-dev-db.sql',
+      // name: 'default',
+      // type: 'sqlite',
+      // database: '../target/db/sqlite-dev-db.sql',
 
-        name: 'shuttle',
-        type: 'postgres',
-        database: 'onnasmdy',
-        host: 'tyke.db.elephantsql.com',
-        port: 5432,
-        username: 'onnasmdy',
-        password: 'uLDLWS0wMgzgF3z4S4wIlvoFH-gm480I',
-        logging: true,
-        synchronize: true,
-        entities: commonConf.ENTITIES,
-        // migrations: commonConf.MIGRATIONS,
-        // // cli: commonConf.CLI,
-        // migrationsRun: commonConf.MIGRATIONS_RUN,
+      name: 'shuttle',
+      type: 'postgres',
+      database: 'defaultdb',
+      host: 'pg-42f8939-pikamsell-3fdc.k.aivencloud.com',
+      port: 18306,
+      username: 'avnadmin',
+      password: 'AVNS_3ANekNkSZAQA7vhn0Oh',
+      logging: true,
+      synchronize: true,
+      entities: commonConf.ENTITIES,
+      // migrations: commonConf.MIGRATIONS,
+      // // cli: commonConf.CLI,
+      // migrationsRun: commonConf.MIGRATIONS_RUN,
     };
-    
-    
+
     // {
     //     // name: 'default',
     //     // type: 'sqlite',
@@ -132,8 +130,8 @@ function ormConfig(): TypeOrmModuleOptions {
     //     // // cli: commonConf.CLI,
     //     migrationsRun: commonConf.MIGRATIONS_RUN,
     // };
-    }
-    return ormconfig;
+  }
+  return ormconfig;
 }
 
 export { ormConfig };
